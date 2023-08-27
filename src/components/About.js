9ecd6c3bf54d89ae35080e1ea94426082d87fd78
Semuel <serg_artemenko@ukr.net>
2023-08-27 23:26:08 +0300

    feat: added content for About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,14 @@
+import { Container } from 'react-bootstrap'
+
 export function About() {
     return ( 
-        <>
-            <h1>About Page</h1>
-        </>
+        <Container>
+            <h1>About us:</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis architecto, veritatis eum dolorem illo natus culpa dicta ea officiis odit eaque cupiditate rerum quidem doloribus fugit id corporis at aut?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum quisquam quos tenetur eaque illo qui distinctio, nemo mollitia! Repudiandae distinctio necessitatibus voluptates a impedit earum corrupti optio temporibus, suscipit maxime.
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus dicta explicabo accusantium, consequuntur adipisci saepe qui nulla distinctio nesciunt fugiat, in quibusdam at voluptas deserunt aliquam incidunt consectetur quisquam commodi?
+            </p>
+        </Container>
     );
 }
