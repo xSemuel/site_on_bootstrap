a5a3cc4918e8c2640de2537907eb0f65330b7be4
Semuel <serg_artemenko@ukr.net>
2023-08-27 21:22:51 +0300

    feat: added Modal window autorisation

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,24 +1,60 @@
-import { Button, Navbar, Nav } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Button, Navbar, Nav, Container, Modal } from "react-bootstrap";
+import { Form, Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const Styles = styled.div `
+    a, .navbar-brand, .navbar-nav .nav-link {
+        color: #abd1b8;
+        &:hover {
+            color: white
+        }
+
+    }
+`
 
 export function NaviBar() {
     return ( 
         <>
+        <Styles>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand>WebDev Blog</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                        <Nav.Link><Link to="/users">Users</Link></Nav.Link>
-                        <Nav.Link><Link to="/about">About</Link></Nav.Link>
-                    </Nav>
-                    <Nav>
-                        <Button variant="primary" className="me-2">Log In</Button>
-                        <Button variant="primary">Sign out</Button>
-                    </Nav>
-                </Navbar.Collapse>
+                <Container>
+                    <Navbar.Brand>WebDev Blog</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                            <Nav.Link><Link to="/users">Users</Link></Nav.Link>
+                            <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Button variant="primary" className="me-2" onClick={handleShow}>Log In</Button>
+                            <Button variant="primary" onClick={handleShow}>Sign out</Button>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
+        </Styles>
+        <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Log in</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Group controlId="fromBasicEmail">
+                        <Form.Label>Email Adress</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">We'll never share your email</Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="fromBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Enter Password" />
+                    </Form.Group>
+                    <Form.Group controlId="fromBasicCheckbox">
+                        <Form.Check type="checkbox" label="Remember me" />
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+        </Modal>
         </>
     );
 }
