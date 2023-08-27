e400d02adad2a43a6f602ffb3d2831489ddb9746
Semuel <serg_artemenko@ukr.net>
2023-08-27 19:49:53 +0300

    feat: added Browser Router in App

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home, Users, About } from './components/About';
 import { NaviBar } from './components/NaviBar'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -5,7 +7,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <>
-      <NaviBar />   
+      <NaviBar />  
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/users" element={<Users />}/>
+          <Route path="/about" element={<About />}/>
+        </Routes>
+      </BrowserRouter> 
     </>
   );
 }
