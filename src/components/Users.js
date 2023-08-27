9d2163136ac5eabb56faffb588a54497b67ae78f
Semuel <serg_artemenko@ukr.net>
2023-08-27 23:23:12 +0300

    feat: ended create Users component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,7 +1,38 @@
+import { Table, Container } from 'react-bootstrap';
+
 export function Users() {
     return ( 
-        <>
-            <h1>Users Page</h1>
-        </>
+        <Container>
+           <Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Username</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>@ndo</td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>dgfdMark</td>
+                        <td>fgdfgOtto</td>
+                        <td>fgf@ndo</td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>dgfdrgfdgMark</td>
+                        <td>fgdfgfdbgOtto</td>
+                        <td>fgf@nggdo</td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Container>
     );
 }
