0aaeb0d5578ba5365a3a8bd77e6aa621291c339b
Semuel <serg_artemenko@ukr.net>
2023-08-28 22:41:44 +0300

    fix: fixed color in footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import { Container } from "react-bootstrap";
 
 export function Footer() {
     return ( 
-        <Container fluid style={{ backgrounColor: '#212529', color: '#fff'}}> 
+        <Container fluid style={{background: 'rgba(var(--bs-dark-rgb)', color: '#fff'}}> 
             <Container style={{ display: 'flex', justifyContent: 'center', padding: '10px'}}>
                 <p>Web Developer Blog</p>
             </Container>
